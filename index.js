2fdabf3680cb36dd42a0db13aa691908cdc883f7
Kacper Makiel <90635502+Kacegz@users.noreply.github.com>
2024-11-13 22:45:44 +0100

    Setup file read and write system

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import OpenAI from "openai";
 import "dotenv/config";
-const openai = new OpenAI({ apiKey: process.env.apikey });
-const completion = await openai.chat.completions.create({
-  model: "gpt-4o",
-  messages: [{ role: "user", content: "write a haiku about ai" }],
-});
-console.log(completion.choices[0].message.content);
+import fs from "fs/promises";
+import path from "path";
+const __dirname = path.resolve();
+
+async function task() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, "text.txt"), {
+      encoding: "utf8",
+    });
+    console.log(data);
+    await fs.writeFile("artykul.html", data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+task();
